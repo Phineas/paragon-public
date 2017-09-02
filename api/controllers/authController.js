381ae05b38d4c67a106405305c7088b6d84053f6
Phineas <phin@phineas.io>
2017-09-02 21:12:35 +0100

    Clean up code

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -5,7 +5,7 @@ exports.get_token = function(req, res) {
   const token = keys.discord,
         ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
-  if(ip == "127.0.0.1" || "localhost" || config.remoteServerAddress) {
+  if(ip === "127.0.0.1" || "localhost" || config.remoteServerAddress) {
     res.json({success: true, token: token});
   } else {
     res.json({success: false, err: "unauthorized"});
